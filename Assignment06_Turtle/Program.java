fcaa715469214e18b684505e16e53d095c2cdf78
tylerkuper <kupert21@asmsa.org>
2019-09-20 12:48:41 -0500

    Updated Turtle Code

--- Assignment06_Turtle/Program.java
+++ Assignment06_Turtle/Program.java
@@ -1,3 +1,11 @@
+/*
+ * Polygon-making turtle program
+ * @ author Tyler Kuper
+ * @ version 1.0
+ * @ since 09-19-19
+ * */
+
+
 import java.util.Scanner;
 public class Program
 {
@@ -8,17 +16,29 @@ public class Program
 			int x;
 			int y;
 			
+			/*Asks the user for an input for the number of sides on the polygon.
+			 *Uses that information to calculate the angles of the polygon to 
+			 *use in the turtleShape function.
+			 * */
 			Scanner in = new Scanner(System.in);
-			System.out.println("Enter an integer for how many sides you want on your polygon.");
+			System.out.println("Enter an integer greater than 2 for how many sides you want on your polygon.");
 			Sides = in.nextInt();
-			Angle = (((Sides - 2) * 180) / Sides);
-			Angle = (180-Angle);
-			Turtle bro;
-			bro = new Turtle();
-			turtleShape(Sides,Angle,bro);
+			if (Sides <= 2) {
+				throw new IllegalArgumentException("The number of sides must be greater than 2");
+			} else {
+				Angle = (((Sides - 2) * 180) / Sides);
+				Angle = (180-Angle);
+				Turtle bro;
+				bro = new Turtle();
+				turtleShape(Sides,Angle,bro);
+			}
 			
 		}
-		
+		/* This function will take the measure of the sides and angles of the entered
+		 * integer and also the turtle that was created to make a recursive way to
+		 * guide the turtle and create the shape the user has input
+		 * 
+		 * */
 		public static void turtleShape(int x,int y,Turtle bro)
 		{
 			if (x < 0){
