53ac48d23a16c1d13c2a8bd6c27c906fc7524031
tylerkuper <kupert21@asmsa.org>
2019-12-06 12:36:58 -0600

    Added csv and data and results rough draft

--- NN_Project/Program.py
+++ NN_Project/Program.py
@@ -1,6 +1,65 @@
+import pandas
+import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.neural_network import MLPClassifier
+
+data=pandas.read_csv("ow.csv",header=None)
+# ~ print(data)
+
+# ~ sns.pairplot( data=data,vars=(0,1,2,3), hue=4 )
+# ~ plt.show()
+
+data=np.array(data)
+
+X=data[:,0:4] 
+y=data[:,4]
+print(X.shape)
+print(y.shape)
+
+
+X,y=shuffle(X,y,random_state=0)
+
+trainX=X[:61]
+trainy=y[:61]
+testX=X[61:]
+testy=y[61:]
+
+
+clf = MLPClassifier(activation='identity', alpha=0.0001, batch_size='auto',
+              beta_1=0.9, beta_2=0.999, early_stopping=False, epsilon=1e-08,
+              hidden_layer_sizes=[3], learning_rate='constant',
+              learning_rate_init=0.001, max_iter=10000, momentum=0.9,
+              n_iter_no_change=10, nesterovs_momentum=True, power_t=0.5,
+              random_state=0, shuffle=True, solver='adam', tol=0.0001,
+              validation_fraction=0.1, verbose=False, warm_start=False)
+clf.fit(trainX, trainy)
+# ~ trainScore=clf.score(trainX,trainy)
+# ~ testScore=clf.score(testX,testy)
+# ~ print("%d,%f,%f"%(layerSize,trainScore,testScore))
+	
+# ~ print(clf.coefs_)
+# ~ prediction=clf.predict(testX)
+
+# ~ #print(prediction)
+
+print(clf.score(trainX,trainy))
+print(clf.score(testX,testy))
+
 from sklearn.model_selection import cross_validate
-from sklearn.model_selection import GridSearchCV
-parameters = ('hidden_layer_sizes':([3], [10], [3,3], [3,3,3]), 'activation':('identity', 'logistic',
-cv_results = GridSearchCV(clf,parameters)
-gs.fit(X,y)
+cv_results = cross_validate(clf, X, y, cv=4)
 print(cv_results)
+
+
+
+
+# ~ from sklearn.model_selection import GridSearchCV
+# ~ parameters = {'hidden_layer_sizes':([3], [10], [3,3], [3,3,3]), 'activation':('identity', 'logistic')}
+# ~ gs = GridSearchCV(clf,parameters)
+# ~ gs.fit(X,y)
+# ~ print(gs.best_estimator_)
+# ~ print(gs.cv_results_)
+
+
